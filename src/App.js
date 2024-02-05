540f1274f813cd1046040f0223e7afb6e9aeab62
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-02-05 20:27:38 +0530

    added notes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import { Counter } from "./Counter";
 import profile from "./profile1.jpeg"
@@ -92,8 +93,8 @@ function App() {
   return (
     <div className="App">
       <div className="product-list">
-        {productList.map((pd) => (
-          <Product product={pd} />
+        {productList.map((pd, index) => (
+          <Product key={index} product={pd} />
         ))}
       </div>
 
@@ -104,23 +105,26 @@ function App() {
 
 function Product({ product }) {
 
-  // const product = {
-  //   name: "Apple iPhone 15 Pro (128 GB) - Natural Titanium",
-  //   price: 130990,
-  //   rating: 5,
-  //   category: "mobile",
-  //   poster: "https://m.media-amazon.com/images/I/81CgtwSII3L._SX679_.jpg",
-  //   summary: "iPhone 15 Pro has a strong and light aerospace-grade titanium design with a textured matte-glass back. It also features a Ceramic Shield front that’s tougher than any smartphone glass. And it’s splash, water, and dust resistant."
-  // }
+  const [show, setShow] = useState(true)
 
+  const ratingStyle = {
+    color: product.rating >= 4.5 ? "green" : "red",
+    fontSize: "20px"
+  }
+  //true  => block => visible
+  //false => none => hide
+  const summaryStyle = {
+    display: show ? "block" : "none"
+  }
   return (
     <div className="product-container">
       <img className="product-poster" src={product.poster} alt={product.name} />
       <div className="product-spec">
         <h5 className="product-name">{product.name}</h5>
-        <p> ⭐{product.rating}</p>
+        <h6 style={ratingStyle}> ⭐{product.rating}</h6>
       </div>
-      <p className="product-summary">{product.summary}</p>
+      <button onClick={() => setShow(!show)}>Toggle Description</button>
+      <p style={summaryStyle} className="product-summary">{product.summary}</p>
       <div className="product-spec">
         <h6>{product.price.toLocaleString('en-IN', { style: 'currency', currency: 'INR' })}</h6>
         <button>Add To Cart</button>
