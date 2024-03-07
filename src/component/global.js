66c9f6ca2f228260e71cd3c4e14abb1933ac0c20
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-03-07 20:47:55 +0530

    updated

--- src/component/global.js
+++ src/component/global.js
@@ -1,1 +1,2 @@
-export const API = "https://659e6ba547ae28b0bd35caec.mockapi.io/product"
+export const API = "https://b55-node.onrender.com/products"
+//"https://659e6ba547ae28b0bd35caec.mockapi.io/product"
